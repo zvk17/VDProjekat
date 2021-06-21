474eaea8724238abeea5635e632f7e78a894e945
zvk17 <25518517+zvk17@users.noreply.github.com>
2021-06-21 23:32:53 +0200

    odradjen deo kreiranja recepta

---
+++ js/Recipe.js
@@ -0,0 +1,24 @@
+class Recipe {
+    constructor(name, duration, description, level) {
+        this.comments = [];
+        this.name = name;
+        this.duration = duration;
+        this.description = description;
+        this.level = level;
+    }
+    static recipes = [];
+    static save() {
+        localStorage.setItem("recipes", JSON.stringify(Recipe.recipes));
+    }
+    static load() {
+        let r = localStorage.getItem("recipes");
+        if (r != null) {
+            r = JSON.parse(r);
+            for (let recipe of recipes) {
+                console.log(recipe);
+                //TODO obraditi ih
+            }
+        }
+    }
+
+}
---
+++ js/addRecipe.js
@@ -0,0 +1,42 @@
+let messages = {
+    MIN_NAME_LENGTH: "Minimalna dužina naslova je 5 znaka.",
+    DESCRIPTION_REQUIRED: "Potrebno je uneti uputstvo za recept",
+    DURATION_REQUIRED: "Obavezno je uneti duzinu spremanja jela"
+}
+$(document).ready(()=>{
+
+    let $dishName = $("#dish-name");
+    let $dishType = $("#dish-type");
+    let $dishLevel = $("#dish-level");
+    let $dishDescription = $("#dish-description");
+    let $dishDuration = $("#dish-duration");
+    let $dishError = $("#dish-error");
+    let $dishSubmit = $("#dish-submit");
+
+    $dishSubmit.on("click", ()=>{
+        let name = $dishName.val();
+        let type = $dishType.val();
+        let level = $dishLevel.val();
+        let description = $dishDescription.val();
+        let duration = $dishDuration.val();
+        let errorMessage = "";
+        if (name.length < 4) {
+            errorMessage += messages.MIN_NAME_LENGTH + "<br />";
+        }
+        if (description.length == 0) {
+            errorMessage += messages.DESCRIPTION_REQUIRED + "<br />";
+        }
+        if (duration.length == 0) {
+            errorMessage += messages.DURATION_REQUIRED + "<br />";
+        }
+        
+        if (errorMessage.length > 0) {
+            
+            $dishError.html(errorMessage);
+            $dishError.removeClass("d-none");
+            return;
+        }
+        $dishError.addClass("d-none");
+        $dishError.empty();
+    });
+});
